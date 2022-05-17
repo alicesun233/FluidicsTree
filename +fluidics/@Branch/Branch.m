650b80c5e88c74e2acaa6067efdb39cb614b3a09
Ziqiang Li <zli11@uwyo.edu>
2022-05-16 22:12:12 -0600

    Basic branch optimization complete but violates a bunch of OOC principles.

--- +fluidics/@Branch/Branch.m
+++ +fluidics/@Branch/Branch.m
@@ -41,13 +41,39 @@ classdef Branch < handle
                 obj.Items = fluidics.core.mat2col([varargin{:}]);
             end
         end
-        
+
+        function delete(obj)
+            delete(obj.Items)
+        end
+
         function item = back(obj)
             item = obj.Items(end);
         end
-        
+
         function pushBack(obj,item)
             obj.Items(end+1,1) = item;
+        end
+
+        function pushChild(obj,branch)
+            obj.Children = [obj.Children;branch];
+            branch.Parents = [branch.Parents;obj];
+        end
+
+        function absorb(obj,branch)
+            % Take ownership of all items
+            obj.Items = [obj.Items;branch.Items];
+            branch.Items = [];
+            % Take custody of all children of the "branch"
+            for b = fluidics.core.mat2row(branch.Children)
+                b.Parents(b.Parents==branch) = obj;
+                obj.Children = [obj.Children;b];
+            end
+            branch.Children(:) = [];
+            % Remove "branch" as children from all its parents
+            for b = fluidics.core.mat2row(branch.Parents)
+                b.Children(b.Children==branch) = [];
+            end
+            branch.Parents(:) = [];
         end
     end
 end
--- +fluidics/tree.m
+++ +fluidics/tree.m
@@ -303,6 +303,299 @@ end
 delete(tempBar)
 frameLimit = tracker.FrameLimit;
 
+% Optimize hanging branches apart by multiple frames
+fprintf('Removing hanging branches...\n')
+CONST_PROXIMITY = 100;
+b = tracker.Branches;
+% Identify branches that are not a child of any branch
+hanging = ~ismember(1:length(b),unique([vertcat(b.Children).ID]));
+ends = [b.FinalItem];
+ends = [ends.Value];
+for i = find(hanging)
+    bi = b(i);
+    bipos = bi.FirstItem.Value.Circumcenter;
+    func = @(c)fluidics.core.dist(c.Circumcenter,bipos);
+    ds = arrayfun(func,ends);
+    selected = bi.FirstFrame>[b.FinalFrame];
+    selected = selected&ds<CONST_PROXIMITY;
+    sel = find(selected);
+    if isempty(sel)
+        continue
+    end
+    selff = [b(sel).FinalFrame];
+    [~,I] = max(selff);
+    connection = sel(I);
+    for j = connection
+        b(j).pushChild(b(i));
+    end
+end
+% Identify branches that do not have a child
+hanging = cellfun(@isempty,{b.Children});
+ends = [b.FirstItem];
+ends = [ends.Value];
+for i = find(hanging)
+    bi = b(i);
+    bipos = bi.FinalItem.Value.Circumcenter;
+    func = @(c)fluidics.core.dist(c.Circumcenter,bipos);
+    ds = arrayfun(func,ends);
+    selected = bi.FinalFrame<[b.FirstFrame];
+    selected = selected&ds<CONST_PROXIMITY;
+    sel = find(selected);
+    if isempty(sel)
+        continue
+    end
+    selff = [b(sel).FirstFrame];
+    [~,I] = min(selff);
+    connection = sel(I);
+    for j = connection
+        b(i).pushChild(b(j));
+    end
+end
+%  Topological analysis begins now
+something_changed = true;
+while something_changed
+    something_changed = false;
+    b = tracker.Branches;
+    % Detect if anything can be shortened
+    for i = 1:length(b)
+        if length(b(i).Children)~=1
+            continue
+        end
+        if length(b(i).Children.Parents)~=1
+            continue
+        end
+        bi = b(i);
+        bj = b(i).Children;
+        fprintf('%d absorbs %d, %d branches left\n',bi.ID,bj.ID,length(b)-1);
+        bi.absorb(bj);
+        tracker.remove(bj);
+        something_changed = true;
+        break
+    end
+    if something_changed
+        continue
+    end
+    % Detect conflicting events
+    for i = 1:length(b)
+        bi = b(i);
+        if length(bi.Children)~=2
+            continue
+        end
+        for bc = fluidics.core.mat2row(bi.Children)
+            if length(bc.Parents)~=2
+                continue
+            end
+            bp = bc.Parents(bc.Parents~=b(i));
+            bo = bi.Children(bi.Children~=bc);
+            if bc.FirstFrame ~= bo.FirstFrame
+                continue
+            end
+            % bi  bc
+            %   ZX
+            % bp  bo
+            fully_connected = ismember(bo,bp.Children);
+            fprintf('            [%4d-%d\n',bi.ID,bc.ID)
+            fprintf('ZX detected:[    ')
+            if fully_connected
+                fprintf('X');
+            else
+                fprintf('Z');
+            end
+            fprintf('\n            [%4d-%d\n',bp.ID,bo.ID)
+
+            % if X, resolve to one of the two configurations
+            % Configuration 1: bi-bc
+            %
+            %                  bp-bo
+            % Configuration 2: bi bc
+            %                    X
+            %                  bp bo
+            % by choosing the one with the smaller sum
+            d1 = fluidics.core.dist(bi.FinalItem.Value.Circumcenter,...
+                bc.FirstItem.Value.Circumcenter)+...
+                fluidics.core.dist(bp.FinalItem.Value.Circumcenter,...
+                bo.FirstItem.Value.Circumcenter);
+            d2 = fluidics.core.dist(bi.FinalItem.Value.Circumcenter,...
+                bo.FirstItem.Value.Circumcenter)+...
+                fluidics.core.dist(bp.FinalItem.Value.Circumcenter,...
+                bc.FirstItem.Value.Circumcenter);
+            if d1<d2
+                % delink bi           bc
+                %          \   and   /
+                %           bo     bp
+                bi.Children(bi.Children==bo) = [];
+                bp.Children(bp.Children==bc) = [];
+                bo.Parents(bo.Parents==bi) = [];
+                bc.Parents(bc.Parents==bp) = [];
+                % link bi-bc and bp-bo
+                bi.Children = unique([bi.Children;bc]);
+                bp.Children = unique([bp.Children;bo]);
+                bo.Parents = unique([bo.Parents;bp]);
+                bc.Parents = unique([bc.Parents;bi]);
+            else
+                % delink bi-bc
+                % and
+                %        bp-bo
+                bi.Children(bi.Children==bc) = [];
+                bp.Children(bp.Children==bo) = [];
+                bc.Parents(bc.Parents==bi) = [];
+                bo.Parents(bo.Parents==bp) = [];
+                % link bi           bc
+                %        \   and   /
+                %         bo     bp
+                bi.Children = unique([bi.Children;bo]);
+                bp.Children = unique([bp.Children;bc]);
+                bo.Parents = unique([bo.Parents;bi]);
+                bc.Parents = unique([bc.Parents;bp]);
+            end
+
+            something_changed = true;
+            break
+        end
+        if something_changed
+            break
+        end
+    end
+    if something_changed
+        continue
+    end
+    % Detect porous branches
+    for i = 1:length(b)
+        bi = b(i);
+        if bi.NumItems/(bi.FinalFrame-bi.FirstFrame+1)>.5
+            continue
+        end
+        for bc = fluidics.core.mat2row(bi.Children)
+            bc.Parents(bc.Parents==bi) = [];
+        end
+        for bp = fluidics.core.mat2row(bi.Parents)
+            bp.Children(bp.Children==bi) = [];
+        end
+        fprintf('Porous branch %d removed, %d branches left\n',bi.ID,length(b)-1)
+        tracker.remove(bi)
+        something_changed = true;
+        if something_changed
+            break
+        end
+    end
+    if something_changed
+        continue
+    end
+    % Remove spurious branches
+    for i = 1:length(b)
+        bi = b(i);
+        if ~isempty(bi.Children)
+            continue
+        end
+        if ~isempty(bi.Parents)
+            continue
+        end
+        fprintf('Spurious branch %d removed, %d branches left\n',bi.ID,length(b)-1)
+        tracker.remove(bi)
+        something_changed = true;
+        if something_changed
+            break
+        end
+    end
+    if something_changed
+        continue
+    end
+    % Merge parallel branches
+    for i = 1:length(b)
+        bi = b(i);
+        if length(bi.Children)<2
+            continue
+        end
+        if length(unique([bi.Children.FirstFrame]))~=1
+            continue
+        end
+        if length(unique([bi.Children.FinalFrame]))~=1
+            continue
+        end
+        fprintf('Merging branches: %d',bi.Children(1).ID)
+        fprintf(', %d',[bi.Children(2:end).ID])
+        fprintf('\n')
+        % Keep the first branch and merge all items of other branches back
+        % into this first branch
+        items = vertcat(bi.Children.Items);
+        items_firstframe = min([items.Frame]);
+        items_finalframe = max([items.Frame]);
+        for j = 1:length(bi.Children)
+            bi.Children(j).Items(:) = [];
+        end
+        for k = items_firstframe:items_finalframe
+            to_merge = [items([items.Frame]==k).Value];
+            to_merge = {to_merge.Points};
+            merged = to_merge{1};
+            while length(to_merge)>1
+                ds = zeros(4,length(to_merge)-1);
+                for j = 2:length(to_merge)
+                    % 1 NOFLIP J NOFLIP
+                    ds(1,j-1) = fluidics.core.dist(merged(end,:),to_merge{j}(1,:));
+                    % 2 NOFLIP J   FLIP
+                    ds(2,j-1) = fluidics.core.dist(merged(end,:),to_merge{j}(end,:));
+                    % 1   FLIP J NOFLIP
+                    ds(3,j-1) = fluidics.core.dist(merged(1,:),to_merge{j}(1,:));
+                    % 2   FLIP J   FLIP
+                    ds(4,j-1) = fluidics.core.dist(merged(1,:),to_merge{j}(end,:));
+                end
+                % Search for the cheapest configuration
+                [~,I] = min(ds,[],'all','linear');
+                [f,j] = ind2sub(size(ds),I);
+                % Concatenate
+                if f==3||f==4
+                    merged = flipud(merged);
+                end
+                if f==1||f==3
+                    % No flip
+                    merged = [merged;to_merge{j+1}];
+                else
+                    % Flip
+                    merged = [merged;flipud(to_merge{j+1})];
+                end
+                to_merge(j+1) = [];
+            end
+            [c,r] = fluidics.core.mincirc(merged);
+            merged_front = struct('Points',      merged,...
+                                  'EdgeCount',   size(merged,1),...
+                                  'Closed',      false,...
+                                  'Circumcenter',c,...
+                                  'Circumradius',r);
+            bi.Children(1).Items = [bi.Children(1).Items;fluidics.Item(merged_front,k)]; 
+        end
+        delete(items)
+        % Remove b(i).Children(2:end) from parents and children
+        for bc = fluidics.core.mat2row(bi.Children(2:end))
+            for bcc = fluidics.core.mat2row(bc.Children)
+                bcc.Parents(bcc.Parents==bc) = [];
+            end
+            tracker.remove(bc)
+        end
+        fprintf('%d branches left\n',bi.ID,length(b)-(length(bi.Children)-1))
+        bi.Children(2:end) = [];
+
+        something_changed = true;
+        if something_changed
+            break
+        end
+    end
+    if something_changed
+        continue
+    end
+    % Remove still branches
+    for i = 1:length(b)
+        bi = b(i);
+        dx = fluidics.core.dist(bi.FirstItem.Value.Circumcenter,...
+                                bi.FinalItem.Value.Circumcenter);
+        dt = bi.FinalFrame-bi.FirstFrame;
+        r = dx/dt;
+        fprintf('%g\n',r)
+    end
+    if something_changed
+        continue
+    end
+end
+
 %%
 fluidics.EvolutionChart('Data',tracker,...
     'Image',im2uint8(maskSolid)/2,...
